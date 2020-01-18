8e9287450fc724535cddc7bf8c14744b21fed149
Su Latt Phone <sulattphone@gmail.com>
2020-01-17 17:30:37 -0800

    Fixing btn and icon effects and topIcon JS

--- mainScript.js
+++ mainScript.js
@@ -1,3 +1,22 @@
+/* Managing the scroll-back-to-top button */
+
+topIcon = document.getElementById("top-icon");
+window.onscroll = function(){topIconVisible()};
+
+// Making the topIcon appear only after scrolling down 66px
+function topIconVisible(){
+    if(document.body.scrollTop > 66 || document.documentElement.scrollTop > 66)
+    {
+        topIcon.style.display = block;
+    }
+    else{
+        topIcon.style.display = none;
+    }
+}
+
+// Scrollling back to top
 function scrollToTop() {
     window.scrollTo(0,0);
 }
+
+/********************************/
