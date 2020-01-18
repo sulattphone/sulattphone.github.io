33b7d0c48483e07c9f2c5c3a6afcd8b01e4304f6
Su Latt Phone <sulattphone@gmail.com>
2020-01-17 18:14:13 -0800

    Hopefully fixed the topIcon mobile view

--- mainScript.js
+++ mainScript.js
@@ -1,16 +1,16 @@
 /* Managing the scroll-back-to-top button */
 
-topIcon = document.getElementById("top-icon");
+topScrollIcon = document.getElementById("top-icon");
 window.onscroll = function(){topIconVisible()};
 
 // Making the topIcon appear only after scrolling down 66px
 function topIconVisible(){
     if(document.body.scrollTop > 66 || document.documentElement.scrollTop > 66)
     {
-        topIcon.style.display = block;
+        topScrollIcon.style.display = block;
     }
     else{
-        topIcon.style.display = none;
+        topScrollIcon.style.display = none;
     }
 }
 
